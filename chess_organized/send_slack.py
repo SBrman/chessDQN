26381477c7f7e63e02260e0432736d122e4cd42d
SBrman <simanta9693@gmail.com>
2022-05-14 21:27:58 -0500

    Delete send_slack.py

--- chess_organized/send_slack.py
+++
@@ -1,8 +0,0 @@
-#! python3
-
-from slack_sdk import WebClient
-
-def send_msg(msg):
-    client = WebClient(token="xoxp-1140170931975-1161092683524-1178761298416-2d25f55c0faa0a460202282ea07a3c63")
-    client.chat_postMessage(channel='#notification', text=msg)
-
